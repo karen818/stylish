110c53997ba2bd0090f8a56248f844314b9b4ca4
karen818 <klc0818@outlook.com>
2016-07-22 11:21:59 -0500

    updates js functions

--- js/app.js
+++ js/app.js
@@ -1,7 +1,54 @@
-function getText(){
+$(function() {
+    console.log( "ready!" );
+
+    $.ajax({
+    type: 'GET',
+    dataType: 'json',
+    url: "http://52.207.252.14:5000/v1.0.0/return_author_probability?txt=blah%20blah%20blah"
+}).then (function(data){
+        var authors = data.results;
+        authors.forEach(function(author){
+            var firstName = author.first_name;
+            var lastName = author.last_name;
+            var probability = author.prob;
+            console.log(author);
+            $("<li></li>").html(firstName + " " + lastName + " " + probability*100).appendTo("#dvContent ul");
+            var authorQueryString = firstName + '%20' + lastName;
+            console.log(authorQueryString);
+            var searchUrl = "https://www.goodreads.com/api/author_url/" + authorQueryString + "?key=7FR037rUMaacRnUp5JJULw";
+            authorQueryRequest = $.ajax({
+                type: "GET",
+                dataType: 'xml',
+                url: searchUrl
+        }).then(function (data) {
+              $(data).find('author').each(function(){
+                //   console.log(data);
+                  var authorID = $(this).attr('id');
+                  var authorLink = $(this).find('link').text();
+                  $("#link").attr('href', authorLink)
+                //   $("<li></li>").html(authorID).appendTo("#dvContent ul");
+                  $.ajax({
+                      type: 'GET',
+                      dataType: 'xml',
+                      url: "https://www.goodreads.com/author/show/" + authorID + "?format=xml&key=7FR037rUMaacRnUp5JJULw"
+                  }).then(function(data){
+                      console.log("done");
+                      var authorPic = $(data).find('author>image_url').html();
+                      authorPic = authorPic.replace("<![CDATA[", "").replace("]]>", "");
+                      console.log(authorPic);
+                      $("<li></li>").html('<img src="' + authorPic + '" />').appendTo("#dvContent ul");
+                    //   $("#picture").attr("src", authorPic);
+                  });
+              });
+          });
+
+        });
+    });
+});
+
+function getUploadText(){
     window.onload = function() {
     		var fileInput = document.getElementById('fileInput');
-    		var fileDisplayArea = document.getElementById('fileDisplayArea');
 
     		fileInput.addEventListener('change', function(e) {
     			var file = fileInput.files[0];
@@ -12,7 +59,6 @@ function getText(){
 
     				reader.onload = function(e) {
                         console.log(reader.result);
-    					fileDisplayArea.innerText = reader.result;
     				}
 
     				reader.readAsText(file);
@@ -22,7 +68,6 @@ function getText(){
     		});
     }
 }
-
 
 
 var app = angular.module("contriApp", []);
@@ -37,22 +82,18 @@ app.controller("TextController", function($scope, $http){
         $scope.newTextUpload = {};
         // newTextUpload.textUpload = ;
         $scope.view.textFiles.push(newTextUpload);
-
-        var reader = new FileReader();
     }
 
-    var textString = getText();
+    var fileString = getUploadText();
     $http({
       method: 'GET',
-      url: "http://52.207.252.14:5000/v1.0.0/return_author_probability?txt=" + textString
+      url: "http://52.207.252.14:5000/v1.0.0/return_author_probability?txt=" + fileString
     }).then(function successCallback(response) {
-        
-        $scope.view.authors = [];
-        var authors = response.data.results;
-        $scope.view.firstName = response.data.results[0].first_name;
-        $scope.view.lastName = response.data.results[0].last_name;
-        $scope.view.prob = response.data.results[0].prob;
-        console.log($scope.view.firstName );
+
+        $scope.view.authorsArray = response.data.results;
+        console.log($scope.view.authorsArray);
+
+
         // this callback will be called asynchronously
         // when the response is available
       }, function errorCallback(response) {
@@ -60,20 +101,6 @@ app.controller("TextController", function($scope, $http){
         // called asynchronously if an error occurs
         // or server returns response with an error status.
       });
-
-
-    $scope.view.authors = [
-        {
-            authorName: "Stephen King",
-            authorUrl: "http://www.writersedit.com/wp-content/uploads/2015/01/stephen-king.jpg",
-            authorBio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            books: [
-                { bookTitle: "It",
-                  bookLink: "https://www.goodreads.com/book/show/830502.It"
-                }
-          ]
-        }
-    ];
 
     $scope.showUploadForm = function(form){
         console.log(form);
@@ -112,3 +139,4 @@ app.controller("TextController", function($scope, $http){
 $(document).ready(function(){
 
 });
+// });
