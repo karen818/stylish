8f2840e884c4696e8f21a6fa5efc45a016c261e5
karen818 <klc0818@outlook.com>
2016-07-27 23:01:22 -0500

    updates app.js file

--- js/app.js
+++ js/app.js
@@ -1,52 +1,58 @@
 
 function getUploadText(){
     window.onload = function() {
-		var fileInput = document.getElementById('fileInput');
+    		var fileInput = document.getElementById('fileInput');
+            var fileSubmit = document.getElementById('uploadBtn');
+    		var fileDisplayArea = document.getElementById('fileDisplayArea');
 
-		fileInput.addEventListener('change', function(e) {
-			var file = fileInput.files[0];
-			var textType = /text.*/;
+    		fileSubmit.addEventListener('click', function(e) {
+    			var file = fileInput.files[0];
+    			var textType = /text.*/;
 
-			if (file.type.match(textType)) {
-				var reader = new FileReader();
+    			if (file.type.match(textType)) {
+    				var reader = new FileReader();
 
-				reader.onload = function(e) {
-                    console.log(reader.result);
-                    return reader.result;
-				}
-				reader.readAsText(file);
-			} else {
-				fileDisplayArea.innerText = "File not supported!"
-			}
-		});
+    				reader.onload = function(e) {
+                        // console.log(reader.result)
+    					return reader.result;
+    				}
+
+    				reader.readAsText(file);
+    			} else {
+    				fileDisplayArea.innerText = "File not supported!"
+    			}
+    		});
     }
+
 }
+// getUploadText();
+
 
 
 var app = angular.module("contriApp", []);
 
 app.controller("TextController", function($scope, $http){
+    var fileString = getUploadText();
+    console.log(fileString);
     $scope.view = {};
     $scope.newTextUpload = {};
     $scope.newTextPaste = {};
     $scope.view.textFiles = [];
 
     $scope.view.showUploadForm = function(){
-        console.log('upload');
         $scope.showUploadForm=!$scope.showUploadForm;
     }
 
     $scope.view.showPasteForm = function(){
-        console.log('paste');
         $scope.showPasteForm=!$scope.showPasteForm;
     }
 
-    $scope.view.uploadText = function(newUpload){
+    $scope.view.uploadText = function(){
         $scope.view.results=!$scope.view.results;
-        var fileString = getUploadText();
+
         $http({
           method: 'POST',
-          url: "http://54.236.208.63:5000/v1.0.0/return_author_probability?txt=" + fileString
+          url: "http://54.236.208.63:5000/v1.0.0/return_author_probability?txt=blah"
         }).then(function successCallback(response) {
             $scope.view.authorsArray = response.data.results;
             var authors = response.data.results;
@@ -54,14 +60,14 @@ app.controller("TextController", function($scope, $http){
                 var firstName = author.first_name;
                 var lastName = author.last_name;
                 var authorQueryString = firstName + '%20' + lastName;
-                var searchUrl = "https://www.goodreads.com/api/author_url/" + authorQueryString + "?key=7FR037rUMaacRnUp5JJULw";
+                var searchUrl = "https://crossorigin.me/https://www.goodreads.com/api/author_url/" + authorQueryString + "?key=7FR037rUMaacRnUp5JJULw";
             $http.get( searchUrl,
                 {transformResponse:function(data) {
                         var x2js = new X2JS();
                         var json = x2js.xml_str2json( data );
                         var authorID = json.GoodreadsResponse.author._id;
-                        console.log(authorID);
-                        var idUrl = "https://www.goodreads.com/author/show/" + authorID + "?format=xml&key=7FR037rUMaacRnUp5JJULw";
+
+                        var idUrl = "https://crossorigin.me/https://www.goodreads.com/author/show/" + authorID + "?format=xml&key=7FR037rUMaacRnUp5JJULw";
                         $http.get(idUrl,
                         {transformResponse:function(data){
                             var x2js = new X2JS();
@@ -89,28 +95,30 @@ app.controller("TextController", function($scope, $http){
       });//here
     }
 
-    $scope.view.pasteText = function(newPaste){
+    $scope.view.pasteText = function(){
         $scope.view.results=!$scope.view.results;
         var fileString = $scope.newPasteUpload;
+        console.log(fileString);
 
         $http({
           method: 'POST',
           url: "http://54.236.208.63:5000/v1.0.0/return_author_probability?txt=" + fileString
         }).then(function successCallback(response) {
             $scope.view.authorsArray = response.data.results;
+            console.log($scope.view.authorsArray);
             var authors = response.data.results;
             authors.forEach(function(author){
                 var firstName = author.first_name;
                 var lastName = author.last_name;
                 var authorQueryString = firstName + '%20' + lastName;
-                var searchUrl = "https://www.goodreads.com/api/author_url/" + authorQueryString + "?key=7FR037rUMaacRnUp5JJULw";
+                var searchUrl = "https://crossorigin.me/https://www.goodreads.com/api/author_url/" + authorQueryString + "?key=7FR037rUMaacRnUp5JJULw";
             $http.get( searchUrl,
                 {transformResponse:function(data) {
                         var x2js = new X2JS();
                         var json = x2js.xml_str2json( data );
                         var authorID = json.GoodreadsResponse.author._id;
                         console.log(authorID);
-                        var idUrl = "https://www.goodreads.com/author/show/" + authorID + "?format=xml&key=7FR037rUMaacRnUp5JJULw";
+                        var idUrl = "https://crossorigin.me/https://www.goodreads.com/author/show/" + authorID + "?format=xml&key=7FR037rUMaacRnUp5JJULw";
                         $http.get(idUrl,
                         {transformResponse:function(data){
                             var x2js = new X2JS();
@@ -124,7 +132,7 @@ app.controller("TextController", function($scope, $http){
                             authorDetails.authorLink = json.GoodreadsResponse.author.link;
 
                             author.authorDetails = authorDetails;
-                            console.log(author.authorDetails);
+
 
                         }
 
